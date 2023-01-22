3ca69db27ef761d391df39d87fff48bbdba5b6b0
shivakumar-p12-sap <96462457+shivakumar-p12-sap@users.noreply.github.com>
2023-01-23 00:46:17 +0530

    Update consolidator.py

--- script/consolidator.py
+++ script/consolidator.py
@@ -4,29 +4,6 @@ import json
 from pathlib import Path
 import fnmatch
 
-json_files = fnmatch.filter(os.listdir("/home/user/logs/k8s/report/"),'*.json')
-
-content = []
-filenames = json_files
-for filename in filenames:
-    with open("/home/user/logs/k8s/report/"+filename, 'r') as f:
-        content += json.load(f)
-with open('/home/user/logs/k8s/report/results.json', 'w') as f:
-    json.dump(content, f, indent=4)
-
-resultInfo = getJsonFromFile('/home/user/logs/k8s/report/results.json')
-filename = '/home/user/logs/k8s/report/index.html'
-templateFilename = "/home/user/config/templates/report/index.html"
-templateFolder = os.path.dirname(templateFilename)
-templateBasename = os.path.basename(templateFilename)
-templateLoader = jinja2.FileSystemLoader(searchpath=templateFolder)
-templateEnv = jinja2.Environment(loader=templateLoader)
-template = templateEnv.get_template(templateBasename)
-with open(filename, 'w') as fh:
-    fh.write(template.render(
-        h1="Cross Consumption Test Results",
-        names=resultInfo
-    ))
 
 def getJsonFromFile(filename, externalConfigAuthMethod=None, externalConfigUserName=None, externalConfigPassword=None, externalConfigToken=None):
     data = None
@@ -67,4 +44,28 @@ def getJsonFromFile(filename, externalConfigAuthMethod=None, externalConfigUserN
             print(message)
         sys.exit(os.EX_DATAERR)
     return data
+
+json_files = fnmatch.filter(os.listdir("/home/user/logs/k8s/report/"),'*.json')
+
+content = []
+filenames = json_files
+for filename in filenames:
+    with open("/home/user/logs/k8s/report/"+filename, 'r') as f:
+        content += json.load(f)
+with open('/home/user/logs/k8s/report/results.json', 'w') as f:
+    json.dump(content, f, indent=4)
+
+resultInfo = getJsonFromFile('/home/user/logs/k8s/report/results.json')
+filename = '/home/user/logs/k8s/report/index.html'
+templateFilename = "/home/user/config/templates/report/index.html"
+templateFolder = os.path.dirname(templateFilename)
+templateBasename = os.path.basename(templateFilename)
+templateLoader = jinja2.FileSystemLoader(searchpath=templateFolder)
+templateEnv = jinja2.Environment(loader=templateLoader)
+template = templateEnv.get_template(templateBasename)
+with open(filename, 'w') as fh:
+    fh.write(template.render(
+        h1="Cross Consumption Test Results",
+        names=resultInfo
+    ))
     
