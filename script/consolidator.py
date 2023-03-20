fd1c5b707e7738cf6dbce835464b8877e057354f
Prabhuswamy <shivakumar.prabhuswamy@sap.com>
2023-03-20 17:39:26 +0530

    Test for status

--- script/consolidator.py
+++ script/consolidator.py
@@ -10,21 +10,28 @@ import datetime as dt
 dt_India = dt.datetime.utcnow() + dt.timedelta(hours=5, minutes=30)
 Indian_time = dt_India.strftime('%d-%b-%y %H:%M:%S')
 
-def check_git_issue(serviceid: str) -> str:
+print(sys.argv[2])
 
-    url = f"https://github.tools.sap/api/v3/search/issues?q=repo:kyma/service-consumption/issues?is%3Aissue+is%3Aopen+%22https%3A%2F%2Fjtrack.wdf.sap.corp%2Fbrowse%2F{serviceid}%22+in%3Asummary"
-
+pagenum=1
+gitissues=[]
+while(True):
+    url=f"https://github.tools.sap/api/v3/search/issues?q=repo:kyma/service-consumption/issues?is%3Aissue+is%3Aopen+%22https%3A%2F%2Fjtrack.wdf.sap.corp%2Fbrowse%2F*%22+in%3Asummary&page={pagenum}&per_page=100"
     headers = {
         'Authorization': 'Bearer '+sys.argv[1],
         'Cookie': 'logged_in=no'
-    }
-
+        }
     response = requests.request("GET", url, headers=headers)
-    data=response.json()
-    if data['total_count'] == 0:
-        return 'none'
-    else:
-        return data['items'][0]['html_url']
+    githubdata=response.json()
+    gitissues+= githubdata["items"]
+    if len(githubdata["items"]) < 100:
+       break
+    pagenum=pagenum+1
+
+def check_git_issue(serviceid: str) -> str:
+  for issue in gitissues:
+      if serviceid in str(issue):
+        return issue["html_url"]
+  return "none" 
 
 def getJsonFromFile(filename, externalConfigAuthMethod=None, externalConfigUserName=None, externalConfigPassword=None, externalConfigToken=None):
     data = None
@@ -77,23 +84,28 @@ for filename in json_files:
     print("IN Loop : "+filename)
     with open("/home/user/logs/k8s/report/"+filename, 'r') as f:
         json_decoded = json.load(f)
-        print('\n'.join(map(str, json_decoded)))
-        print(json.dumps(json_decoded))
-        logfile = filename.replace(".json", ".log" )
-        print("CRETED log file name : "+logfile)
-        json_decoded[0]['loglink']='https://github.tools.sap/BTP-E2EScenarioValidation/crossconsumption-report/blob/main/logs/'+logfile
-        json_decoded[0]['githubissue']=check_git_issue(json_decoded[0]['serviceid'])
-        source_logfile="/home/user/logs/k8s/report/"+logfile
-        with open(source_logfile, 'r') as file:
-          log_content = file.read()
-          json_decoded[0]['deleteStatus']='Failed'
-          if 'is now available' in log_content:
-            json_decoded[0]['creationStatus']='Pass'
-          else:
-            json_decoded[0]['creationStatus']='Failed'
-          if 'all service instances now deleted' in log_content:
-            json_decoded[0]['deleteStatus']='Pass'
-        content += json_decoded
+        print(len(json_decoded))
+        if len(json_decoded) > 0:
+          logfile = filename.replace(".json", ".log")
+          print("CRETED log file name : "+logfile)
+          json_decoded[0]['loglink']='https://github.tools.sap/BTP-E2EScenarioValidation/crossconsumption-report/blob/main/logs/'+logfile
+          print("LogLINK : "+json_decoded[0]['loglink'])
+          print("SERVICE ID : "+json_decoded[0]['serviceid'])
+          json_decoded[0]['githubissue']=check_git_issue(json_decoded[0]['serviceid'])
+          print("githubissue : "+json_decoded[0]['githubissue'])
+          source_logfile="/home/user/logs/k8s/report/"+logfile
+          with open(source_logfile, 'r') as file:
+            log_content = file.read()
+            json_decoded[0]['deleteStatus']='Failed'
+            if 'is now available' in log_content:
+              json_decoded[0]['creationStatus']='Pass'
+            else:
+              json_decoded[0]['creationStatus']='Failed'
+            if 'all service instances now deleted' in log_content:
+              json_decoded[0]['deleteStatus']='Pass'
+          content += json_decoded
+        else:
+            print("SKIP : "+filename)
 with open('/home/user/logs/k8s/report/results.json', 'w') as f:
     json.dump(content, f, indent=4)
 
